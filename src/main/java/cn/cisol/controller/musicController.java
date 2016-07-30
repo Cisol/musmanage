2bc3e35e133c5d769cf5cd0b551e2b17c73ff1ba
z0608 <zhangm@dtdream.com>
2016-07-30 17:39:09 +0800

    modified

---
+++ src/main/java/cn/cisol/controller/musicController.java
@@ -0,0 +1,7 @@
+package cn.cisol.controller;
+
+/**
+ * Created by Administrator on 2016/7/30 0030.
+ */
+public class musicController {
+}
---
+++ src/main/java/cn/cisol/entity/musicEntity.java
@@ -0,0 +1,7 @@
+package cn.cisol.entity;
+
+/**
+ * Created by Administrator on 2016/7/30 0030.
+ */
+public class musicEntity {
+}
